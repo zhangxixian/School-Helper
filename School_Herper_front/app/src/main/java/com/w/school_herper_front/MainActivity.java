f4cf3f503071d0b8b9196990f314880000fba48d
shangyifei666 <1004978291@qq.com>
2018-11-23 08:45:42 +0800

    新建前端文件
    前端文件用Android studio使用，后台用eclipse使用

---
+++ School_Herper_front/app/src/main/java/com/w/school_herper_front/MainActivity.java
@@ -0,0 +1,13 @@
+package com.w.school_herper_front;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
