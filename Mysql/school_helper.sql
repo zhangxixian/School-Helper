1e89e86402fbb9007d39081862cefc86b517236d
zhang <2191791476@qq.com>
2018-11-27 11:12:16 +0800

    Signed-off-by: zhang <2191791476@qq.com>

---
+++ Mysql/school_helper.sql
@@ -0,0 +1,78 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : zhang
+Source Server Version : 80013
+Source Host           : localhost:3306
+Source Database       : school_helper
+
+Target Server Type    : MYSQL
+Target Server Version : 80013
+File Encoding         : 65001
+
+Date: 2018-11-27 11:09:46
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for `contaction`
+-- ----------------------------
+DROP TABLE IF EXISTS `contaction`;
+CREATE TABLE `contaction` (
+  `release_user_id` int(20) NOT NULL,
+  `receive_user_id` int(20) NOT NULL,
+  `reward_id` int(10) NOT NULL,
+  KEY `reward_id` (`reward_id`),
+  CONSTRAINT `contaction_ibfk_1` FOREIGN KEY (`reward_id`) REFERENCES `reward` (`reward_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of contaction
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `reward`
+-- ----------------------------
+DROP TABLE IF EXISTS `reward`;
+CREATE TABLE `reward` (
+  `reward_id` int(10) NOT NULL,
+  `release_user_id` int(20) NOT NULL,
+  `reward_content` varchar(255) NOT NULL,
+  `reward_key` varchar(20) NOT NULL,
+  `reward_money` decimal(10,2) NOT NULL,
+  `reward_time` timestamp NOT NULL,
+  `reward_deadline` timestamp NOT NULL,
+  `reward_receive_count` int(10) NOT NULL,
+  `reward_state` varchar(10) NOT NULL,
+  PRIMARY KEY (`reward_id`),
+  UNIQUE KEY `reward_id` (`reward_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of reward
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `user`
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `user_id` int(20) NOT NULL,
+  `user_name` varchar(25) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `user_password` varchar(25) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `user_school` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `user_student_num` int(20) NOT NULL,
+  `user_phone` int(20) NOT NULL,
+  `user_image` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `user_money` decimal(10,2) NOT NULL,
+  `user_reputation_value` int(10) NOT NULL,
+  `user_took_count` int(10) NOT NULL,
+  `ueser_publish_count` int(10) unsigned zerofill NOT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `user_id` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
